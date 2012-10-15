3749c0d461780a9a1264aa28e4bcb7c3e3d9143b
CirnoTheGenius <cirnothegenius@aol.com>
2012-10-14 18:46:35 -0700

    Fixed up Channels.
    Moved around the addPlayer function and the removePlayer.
    Fied up the isCommonName function.
    Added null check for ChatColor c.

--- src/toshir0z/Cirno/Channel.java
+++ src/toshir0z/Cirno/Channel.java
@@ -18,33 +18,68 @@ public class Channel {
 		core = n;
 	}
 	
-	public void addPlayer(String p){
-		Chatters.add(p);
-		announce(p + " has joined the channel!", ChatColor.YELLOW);
-	}
-	
-	public void removePlayer(String p){
+	private boolean isCommonName(String p, ArrayList<String> al){
 		short i = 0;
-		for(String s : Chatters){
+		for(String s : al){
 			if(s.startsWith(p)){
 				i++;
 			}
 		}
 		if(i > 1){
-			announceAdmins("Failed to kick player \"" + p + "\"!", ChatColor.RED);
+			return true;
 		}
+		return false;
+	}
+	
+	public void addPlayer(String p){
+		Chatters.add(core.getServer().getPlayer(p).getName());
 		announce(p + " has joined the channel!", ChatColor.YELLOW);
+	}
+	
+	public void removePlayer(String p){
+		if(isCommonName(p, Chatters)){
+			announceAdmins("Failed to kick player \"" + p + "\"! Reason: Multiple players exist with that name!", ChatColor.RED);
+			return;
+		}
+		
+		String playerName = core.getServer().getPlayer(p).getName();
+		
+		if(!Chatters.contains(playerName)){
+			announceAdmins("Failed to kick player \"" + p + "\"! Reason: Player \"" + p + "\" not exist!", ChatColor.RED);
+			return;
+		}
+		Chatters.remove(playerName);
+		announce(p + " has been kicked from the channel!", ChatColor.YELLOW);
+	}
+	
+	public void adminPlayer(String p){
+		if(isCommonName(p, Chatters)) {
+			announceAdmins("Failed to admin player \"" + p + "\"! Reason: Multiple players exist with that name!", ChatColor.RED);
+			return;
+		}
+		
+		String playerName = core.getServer().getPlayer(p).getName();
+		
+		if(!Chatters.contains(playerName)){
+			announceAdmins("Failed to admin player \"" + p + "\"! Reason: Player \"" + p + "\" is not in the channel!", ChatColor.RED);
+			return;
+		} else if(Admins.contains(playerName)){
+			announceAdmins("Failed to admin player \"" + p + "\"! Reason: Player already an admin!", ChatColor.RED);
+			return;
+		}
+		Admins.add(core.getServer().getPlayer(p).getName());
+		announceAdmins("Player \"" + p + "\" is now an admin!", ChatColor.GREEN);
 	}
 	
 	public void announce(String m, ChatColor c){
 		for(String s : Chatters){
-			core.getServer().getPlayer(s).sendMessage(c.getClass() != ChatColor.class ? "" : c + "[Channel: " + name + "] " + m);
+			core.getServer().getPlayer(s).sendMessage(c == null || c.getClass() != ChatColor.class ? "" : c + "[Channel: " + name + "] " + m);
 		}
 	}
 	
 	public void announceAdmins(String m, ChatColor c){
 		for(String s : Admins){
-			core.getServer().getPlayer(s).sendMessage(c.getClass() != ChatColor.class ? "" : c + "[Channel: " + name + "] " + m);
+			core.getServer().getPlayer(s).sendMessage(c == null || c.getClass() != ChatColor.class ? "" : c + "[Channel: " + name + "] " + m);
 		}
 	}
 	
